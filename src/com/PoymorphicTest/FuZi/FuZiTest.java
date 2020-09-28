3222e84f0154f89e1c92d651abb66f837656d36c
hbc110 <1159203717@qq.com>
2020-09-28 17:24:51 +0800

    @me@PoymorphicTest_FuZi,添加注释(父类引用指向子类的对象).

--- src/com/PoymorphicTest/FuZi/FuZiTest.java
+++ src/com/PoymorphicTest/FuZi/FuZiTest.java
@@ -4,9 +4,9 @@ public class FuZiTest {
     //类的多态使用
     public static void main(String[] args) {
 
-        //类的多态使用
+        //类的多态使用即父类引用指向子类的对象
         Fu f =new Zi();
-        
+
         //接口的多态使用
         IFu ifu = new Zi();
         ifu.Imethod();
